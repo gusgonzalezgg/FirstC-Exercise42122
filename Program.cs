cdb8e505e179bec08ff00bf37c9d3b820d2f78a9
Gustavo Gonzalez <gusgonzalez.gg@gmail.com>
2022-04-21 10:24:16 -0400

    first commit, added some lines

---
+++ Program.cs
@@ -0,0 +1,4 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+Console.WriteLine("This is the first C# Excercise, making the slightest bit more sense");
+Console.WriteLine("Only the 4th day, trusting the process");
